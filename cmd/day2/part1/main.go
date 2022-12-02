384068d9227dac55294510c24688eb13cf721f00
Karl Hepler <karl.hepler@gmail.com>
2022-12-02 07:38:20 -0500

    solved day 2

---
+++ cmd/day2/part1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/karlhepler/advent-of-code-2022/fyl"
+	"github.com/karlhepler/advent-of-code-2022/hlp"
+)
+
+func main() {
+	var score int
+	var scoremap = map[string]int{
+		"A X": 1 + 3, // rock rock (draw)
+		"B X": 1 + 0, // paper rock (lose)
+		"C X": 1 + 6, // scissors rock (win)
+		"A Y": 2 + 6, // rock paper (win)
+		"B Y": 2 + 3, // paper paper (draw)
+		"C Y": 2 + 0, // scissors paper (lose)
+		"A Z": 3 + 0, // rock scissors (lose)
+		"B Z": 3 + 6, // paper scissors (win)
+		"C Z": 3 + 3, // scissors scissors (draw)
+	}
+
+	filepath := "cmd/day2/input.txt"
+	hlp.Must(fyl.ReadEachLine(filepath, func(line string) error {
+		score += scoremap[line]
+		return nil
+	}))
+
+	fmt.Println(score)
+}
---
+++ cmd/day2/part2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/karlhepler/advent-of-code-2022/fyl"
+	"github.com/karlhepler/advent-of-code-2022/hlp"
+)
+
+func main() {
+	// X == lose
+	// Y == draw
+	// Z == win
+
+	var score int
+	var scoremap = map[string]int{
+		"A X": 3 + 0, // rock (lose) = scissors
+		"B X": 1 + 0, // paper (lose) = rock
+		"C X": 2 + 0, // scissors (lose) = paper
+		"A Y": 1 + 3, // rock (draw) = rock
+		"B Y": 2 + 3, // paper (draw) = paper
+		"C Y": 3 + 3, // scissors (draw) = scissors
+		"A Z": 2 + 6, // rock (win) = paper
+		"B Z": 3 + 6, // paper (win) = scissors
+		"C Z": 1 + 6, // scissors (win) = rock
+	}
+
+	filepath := "cmd/day2/input.txt"
+	hlp.Must(fyl.ReadEachLine(filepath, func(line string) error {
+		score += scoremap[line]
+		return nil
+	}))
+
+	fmt.Println(score)
+}
