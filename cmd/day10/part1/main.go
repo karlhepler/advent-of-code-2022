d151485108802a9cbadbd494e93204602340c465
Karl Hepler <karl.hepler@gmail.com>
2022-12-12 16:50:00 -0500

    init day10

--- cmd/day10/part1/main.go
+++ cmd/day10/part1/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/karlhepler/advent-of-code-2022/fyl"
+	"github.com/karlhepler/advent-of-code-2022/it"
+)
+
 func main() {
-	//
+	filepath := "cmd/day10/example1"
+	it.Must(fyl.ReadEachLine(filepath, func(line []byte) error {
+		return nil
+	}))
 }
