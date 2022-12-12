6a6d23216f7c92e8c6562fc235a4acc96861a705
Karl Hepler <karl.hepler@gmail.com>
2022-12-12 10:11:07 -0500

    day9 part2 refactor parity with example2

--- cmd/day9/part2/main.go
+++ cmd/day9/part2/main.go
@@ -15,7 +15,7 @@ func main() {
 	var bridge = &Bridge{}
 	var visits = make(map[[2]float64]bool)
 
-	filepath := "cmd/day9/example"
+	filepath := "cmd/day9/input"
 	it.Must(fyl.ReadEachLine(filepath, func(line []byte) error {
 		split := strings.Split(string(line), " ")
 		numSteps := it.Must2(strconv.Atoi(split[1]))
@@ -61,8 +61,8 @@ func (b *Bridge) Step(direction string) error {
 		if err != nil {
 			return nil
 		}
-		b.Tail = next
 
+		b.Tail = next
 	}
 }
 
