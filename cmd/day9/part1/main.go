b459bf69a603441791ea4805b7cee02d56221d9f
Karl Hepler <karl.hepler@gmail.com>
2022-12-12 09:26:46 -0500

    day9 part1 example works

--- cmd/day9/part1/main.go
+++ cmd/day9/part1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
@@ -12,6 +13,7 @@ import (
 
 func main() {
 	var bridge = &Bridge{}
+	var visits = make(map[[2]float64]bool)
 
 	filepath := "cmd/day9/example"
 	it.Must(fyl.ReadEachLine(filepath, func(line []byte) error {
@@ -23,17 +25,19 @@ func main() {
 			if err != nil {
 				return err
 			}
-		}
 
-		fmt.Printf("%+v\n", bridge)
+			visits[bridge.Tail] = true
+		}
 
 		return nil
 	}))
+
+	fmt.Println(len(visits))
 }
 
 type Bridge struct {
-	Head Vector
-	Tail Vector
+	Head [2]float64
+	Tail [2]float64
 }
 
 func (b *Bridge) Step(direction string) error {
@@ -50,7 +54,35 @@ func (b *Bridge) Step(direction string) error {
 		return errors.New("invalid direction: " + direction)
 	}
 
+	for {
+		dx := b.Head[0] - b.Tail[0]
+		dy := b.Head[1] - b.Tail[1]
+
+		delta := math.Sqrt(dx*dx + dy*dy)
+		unit := [2]float64{dx / delta, dy / delta}
+
+		// fmt.Printf("delta=%v; udx=%v\n", delta, unit)
+		if delta < 2 {
+			break
+		}
+
+		b.Tail[0] += sign(unit[0])
+		b.Tail[1] += sign(unit[1])
+	}
+
+	// fmt.Printf("%+v\n", b)
+
+	// fmt.Println("---")
+
 	return nil
 }
 
-type Vector [2]float64
+func sign(a float64) float64 {
+	if a > 0 {
+		return 1
+	}
+	if a < 0 {
+		return -1
+	}
+	return 0
+}
