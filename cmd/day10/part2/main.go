d1073d95cf6890b2b7b7e56c9a4f53a341b4b434
Karl Hepler <karl.hepler@gmail.com>
2022-12-13 11:35:40 -0500

    day10 part2 example2 works

---
+++ cmd/day10/part2/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/karlhepler/advent-of-code-2022/fyl"
+	"github.com/karlhepler/advent-of-code-2022/it"
+)
+
+const Width = 40
+
+func main() {
+	var vals []int
+
+	filepath := "cmd/day10/example2"
+	it.Must(fyl.ReadEachLine(filepath, func(line []byte) error {
+		cmdval := strings.Split(string(line), " ")
+
+		// noop takes one cycle and does nothing
+		if cmdval[0] == "noop" {
+			vals = append(vals, 0)
+			return nil
+		}
+
+		// addx takes two cycles and adds the value to x
+		vals = append(vals, 0, it.Must2(strconv.Atoi(cmdval[1])))
+
+		return nil
+	}))
+
+	var xreg int = 1
+	var crty int
+	for crtx, val := range vals {
+		nextCrty := int(crtx / Width)
+		if nextCrty > crty {
+			crty = nextCrty
+			fmt.Print("\n")
+		}
+
+		fmt.Print(pixel(crtx%Width, xreg))
+
+		xreg += val
+	}
+
+	fmt.Print("\n")
+}
+
+func pixel(crtx, spritex int) string {
+	if crtx == spritex-1 || crtx == spritex || crtx == spritex+1 {
+		return "#"
+	}
+	return "."
+}
