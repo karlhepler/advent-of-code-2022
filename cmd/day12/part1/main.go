0a7e0f876a5d7435cf3b9306d888e247204eba24
Karl Hepler <karl.hepler@gmail.com>
2022-12-15 16:36:43 -0500

    constructed map and got start and end coords

--- cmd/day12/part1/main.go
+++ cmd/day12/part1/main.go
@@ -6,15 +6,34 @@ import (
 )
 
 func main() {
-	var start, end [2]int    // [row, col]
-	var heightmap [][]string // [row][col]
+	var start, end [2]int  // [row, col]
+	var heightmap [][]byte // [row][col]
 
 	filepath := "cmd/day12/example"
 	it.Must(fyl.ReadEachLine(filepath, func(line []byte) error {
-		if line[0] == 'S' {
-			line[0] = 'a'
+		if col, ok := indexOf(line, 'S'); ok {
+			row := len(heightmap)
+			start = [2]int{row, col}
+			line[col] = 'a'
 		}
+		if col, ok := indexOf(line, 'E'); ok {
+			row := len(heightmap)
+			end = [2]int{row, col}
+			line[col] = 'z'
+		}
+
+		heightmap = append(heightmap, line)
 
 		return nil
 	}))
+}
+
+func indexOf(line []byte, a byte) (i int, ok bool) {
+	for i, c := range line {
+		if c == a {
+			return i, true
+		}
+	}
+
+	return -1, false
 }
